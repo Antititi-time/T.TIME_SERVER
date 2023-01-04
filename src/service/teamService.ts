092490fd3455b5e075e476c1ec38959e41bb71f7
Minjae Kang <rkdalswo1021@naver.com>
2023-01-05 01:50:24 +0900

    add: teamService 작성

--- src/service/teamService.ts
+++ src/service/teamService.ts
@@ -51,7 +51,60 @@ const participateTeam = async (nickname: string, teamCode: string) => {
     throw error;
   }
 };
+
+const checkTeamHappiness = async (teamCode: string) => {
+  try {
+    const checkTeamId = await prisma.team.findFirst({
+      where: {
+        team_code: teamCode,
+      },
+    });
+    if (!checkTeamId) {
+      return null;
+    }
+
+    const checkHappiness = await prisma.team_user.findMany({
+      where: {
+        team_id: checkTeamId.id,
+        is_completed: true,
+      },
+      select: {
+        is_completed: true,
+      },
+    });
+
+    const teamInfo = await prisma.team.findFirst({
+      where: {
+        id: checkTeamId.id,
+      },
+      select: {
+        team_member: true,
+      },
+    });
+    if (!teamInfo) {
+      return null;
+    }
+    if (checkHappiness.length == teamInfo.team_member) {
+      const result = {
+        completed: true,
+        completedNumber: checkHappiness.length,
+        totalNumber: teamInfo.team_member,
+      };
+      return result;
+    }
+    const result = {
+      completed: false,
+      completedNumber: checkHappiness.length,
+      totalNumber: teamInfo.team_member,
+    };
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 export default {
   makeTeam,
   participateTeam,
+  checkTeamHappiness,
 };
