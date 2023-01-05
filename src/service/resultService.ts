d97c7d3b7c3ca90c8403541511d61e2f3f0952bf
Minjae Kang <rkdalswo1021@naver.com>
2023-01-05 23:08:42 +0900

    add: 팀 결과 항목별 점수 조회 service 작성

--- src/service/resultService.ts
+++ src/service/resultService.ts
@@ -59,59 +59,42 @@ const userResult = async (userId: number) => {
   }
 };
 
-// const teamResultByType = async (teamCode: string) => {
-//   try {
-//     const findTeam = await prisma.team.findFirst({
-//       where: {
-//         team_code: teamCode,
-//       },
-//     });
-//     if (!findTeam) {
-//       return null;
-//     }
-
-//     const scores = await prisma.chat.groupBy({
-//       by: ['question_type'],
-//       where: {
-//         team_id: findTeam.id,
-//       },
-//       _sum: {
-//         grade: true,
-//       },
-//       orderBy: {
-//         _sum: {
-//           grade: 'desc',
-//         },
-//       },
-//     });
-//     if (!scores) {
-//       return null;
-//     }
-//     const scoreResult = await Promise.all(
-//       scores.map((score: any) => {
-//         const result = {
-//           grade: score._sum.grade,
-//           questionType: score.question_type,
-//         };
-//         return result;
-//       }),
-//     );
-
-//     const data = {
-//       date: dayjs(findTeam.team.updatedAt).format('YYYY-MM-DD'),
-//       nickname: findTeam.nickname.name,
-//       teamName: findTeam.team.team_name,
-//       result: scoreResult,
-//     };
-
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
+const teamResultByType = async (teamId: number) => {
+  try {
+    const scores = await prisma.chat.groupBy({
+      by: ['question_type'],
+      where: {
+        team_id: teamId,
+      },
+      _sum: {
+        grade: true,
+      },
+      orderBy: {
+        _sum: {
+          grade: 'desc',
+        },
+      },
+    });
+    if (!scores) {
+      return null;
+    }
+    const scoreResult = await Promise.all(
+      scores.map((score: any) => {
+        const result = {
+          grade: score._sum.grade,
+          questionType: score.question_type,
+        };
+        return result;
+      }),
+    );
+    return scoreResult;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export default {
   userResult,
-  // teamResultByType,
+  teamResultByType,
 };
